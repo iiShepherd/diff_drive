fd23acda455d1155ef099455d221666a7cefb9f8
Eric <HuangX0206@GMail.com>
2018-07-29 23:15:24 -0400

    >:)

--- src/joystick.cpp
+++ src/joystick.cpp
@@ -1,14 +1,16 @@
+#include <ros/ros.h>
 #include <iostream>
 #include <string>
 #include <SDL2/SDL.h>
-#include <ros/ros.h>
+#include <thread>
+#include <chrono>
 #include  "robo/joystickcmd.h"
 
 Uint32 timeLastMs = 0;
 Uint32 timeCurrentMs  = 0;
 Uint32 timeDeltaMs = 0;
 Uint32 timeAccumulatedMs = 0;
-Uint32 timeStepMs = 1000.f / 60;
+Uint32 timeStepMs = 1000.f / 120;
 
 
 
@@ -22,9 +24,8 @@ struct var
 	//ROS
 	ros::Publisher joy_pub;
 
-	//array
+	//custom message
 	robo::joystickcmd joystick_cmd;
-	unsigned int num_det;
 	
 
 }param;
@@ -37,53 +38,33 @@ void cmd_foward(SDL_Joystick* joystick)
 
 	param.joystick_cmd.jaxis_1 = SDL_JoystickGetAxis(joystick, 1);
 	param.joystick_cmd.jbutton_0 = SDL_JoystickGetButton(joystick, 0);
-
+	std::this_thread::sleep_for( std::chrono::milliseconds(1) );
 	
 }
 
 int main(int argc, char** argv)
 {
-	//initiattion
+	//initiation
 	ros::init(argc, argv, "Joymotion");
 	ros::NodeHandle nh;
 
 	SDL_Init(SDL_INIT_JOYSTICK);
 	SDL_Joystick *joystick = SDL_JoystickOpen(0);
 
+	param.joy_pub = nh.advertise<robo::joystickcmd>("/joystick/motion_cmd", 50);
 
+		timeLastMs = timeCurrentMs;
+		timeCurrentMs = SDL_GetTicks();
+		timeDeltaMs = timeCurrentMs - timeLastMs;
+		timeAccumulatedMs += timeDeltaMs;
 
+		while (ros::ok())
+		{
+		cmd_foward(joystick);
+		param.joy_pub.publish(param.joystick_cmd);
+		ros::spinOnce();
+		}		
 
-
-
-
-		//timeLastMs = timeCurrentMs;
-		//timeCurrentMs = SDL_GetTicks();
-		//timeDeltaMs = timeCurrentMs - timeLastMs;
-		//timeAccumulatedMs += timeDeltaMs;
-
-		//while (timeAccumulatedMs >= timeStepMs)
-		//{
-	if(SDL_JoystickOpen(0))
-{
-		ROS_INFO("JOYSTICK CONNECTED");
-		while(SDL_JoystickOpen(0)){
-
-			//cmd_foward(joystick);
-	SDL_JoystickUpdate();
-	SDL_PumpEvents();
-
-	param.joystick_cmd.jaxis_1 = SDL_JoystickGetAxis(joystick, 1);
-	param.joystick_cmd.jbutton_0 = SDL_JoystickGetButton(joystick, 0);
-
-			param.joy_pub = nh.advertise<robo::joystickcmd>("/joystick/motion_cmd", 100);
-			ros::spinOnce();
-			}
-	
-}
-		else
-{
-ROS_INFO("PLEASE CONNECT A JOYSTICK");
-}
-	ROS_INFO(" EXITING..." );
-	return 0;
+		ROS_INFO(" EXITING..." );
+		return 0;
 }
