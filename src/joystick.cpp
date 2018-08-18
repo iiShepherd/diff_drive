4d66d1b75185b2077b149d87c6a60c511f065822
Eric <HuangX0206@GMail.com>
2018-08-18 16:58:53 -0400

    testing rosserial

--- src/joystick.cpp
+++ src/joystick.cpp
@@ -6,11 +6,11 @@
 #include <chrono>
 #include  "robo/joystickcmd.h"
 
-Uint32 timeLastMs = 0;
-Uint32 timeCurrentMs  = 0;
-Uint32 timeDeltaMs = 0;
-Uint32 timeAccumulatedMs = 0;
-Uint32 timeStepMs = 1000.f / 120;
+//Uint32 timeLastMs = 0;
+//Uint32 timeCurrentMs  = 0;
+//Uint32 timeDeltaMs = 0;
+//Uint32 timeAccumulatedMs = 0;
+//Uint32 timeStepMs = 1000.f / 120;
 
 
 
@@ -33,6 +33,7 @@ void cmd_foward(SDL_Joystick* joystick)
 	SDL_PumpEvents();
 
 	param.joystick_cmd.jaxis_1 = SDL_JoystickGetAxis(joystick, 1);
+	param.joystick_cmd.jaxis_0 = SDL_JoystickGetAxis(joystick, 0);
 	param.joystick_cmd.jbutton_0 = SDL_JoystickGetButton(joystick, 0);
 	std::this_thread::sleep_for( std::chrono::milliseconds(100) );
 	
@@ -46,13 +47,17 @@ int main(int argc, char** argv)
 
 	SDL_Init(SDL_INIT_JOYSTICK);
 	SDL_Joystick *joystick = SDL_JoystickOpen(0);
+	if(joystick == NULL)
+	{
+		ROS_INFO(" NO JOYSTICK FOUND" );
+	}
 
 	param.joy_pub = nh.advertise<robo::joystickcmd>("/joystick/motion_cmd", 50);
 
-		timeLastMs = timeCurrentMs;
-		timeCurrentMs = SDL_GetTicks();
-		timeDeltaMs = timeCurrentMs - timeLastMs;
-		timeAccumulatedMs += timeDeltaMs;
+		//timeLastMs = timeCurrentMs;
+		//timeCurrentMs = SDL_GetTicks();
+		//timeDeltaMs = timeCurrentMs - timeLastMs;
+		//timeAccumulatedMs += timeDeltaMs;
 
 		while (ros::ok())
 		{
